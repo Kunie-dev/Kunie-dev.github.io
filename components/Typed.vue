f89e4e1470404fd6220e679dc1c2e767583fd1f2
Kunie <kunie.dev@gmail.com>
2022-10-11 00:00:50 +0900

    + Typed.vue

---
+++ components/Typed.vue
@@ -0,0 +1,22 @@
+<template>
+  <vue-typed-js :strings="strings" :loop="true" :typeSpeed="100" :backSpeed="50">
+    <span class="typing"></span>
+  </vue-typed-js>
+</template>
+
+<script>
+import { VueTypedJs } from "vue-typed-js";
+
+export default {
+  name: "Typed",
+  components: {
+    VueTypedJs
+  },
+  props: {
+    strings: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
