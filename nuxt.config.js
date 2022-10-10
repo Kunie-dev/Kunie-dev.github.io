c1be08c6e7aa31e93d8f4b86936adf4aa897caa6
Kunie <kunie.dev@gmail.com>
2022-10-11 00:04:36 +0900

    change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'kunie-portfolio-site',
+    title: 'Kunie Portfolio',
     htmlAttrs: {
       lang: 'ko',
     },
