8a3d0d7135ff7a1abd04dbd60d761f3b08e9cbd8
Kunie <kunie.dev@gmail.com>
2022-10-11 11:14:14 +0900

    Navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,16 @@
 <template>
-  <Nuxt />
+  <div>
+    <b-navbar tag="header" fixed="top">
+      <b-navbar-brand href="#">Kunie</b-navbar-brand>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#home">Home</b-nav-item>
+        <b-nav-item href="#about">About</b-nav-item>
+        <b-nav-item href="#skills">Skills</b-nav-item>
+        <b-nav-item href="#portfolio">Portfolio</b-nav-item>
+        <b-nav-item href="#blog">Blog</b-nav-item>
+        <b-nav-item href="#contact">Contact</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+    <Nuxt />
+  </div>
 </template>
