36cc0acfb01d4d7a3b08039a530782dce3282311
Kunie <kunie.dev@gmail.com>
2022-10-13 09:58:08 +0900

    navbar alignment

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,27 +2,34 @@
   <div>
     <b-navbar tag="header" class="header" fixed="top" v-b-scrollspy>
       <b-navbar-brand href="#">KUNIE</b-navbar-brand>
-      <b-navbar-nav class="ml-auto gnb">
+      <b-navbar-nav class="gnb">
         <b-nav-item href="#home"
-          ><b-icon icon="house-door-fill" /><span>Home</span></b-nav-item
+        ><b-icon icon="house-door-fill" /><span>Home</span></b-nav-item
         >
         <b-nav-item href="#about"
-          ><b-icon icon="person-lines-fill" /><span>About</span></b-nav-item
+        ><b-icon icon="person-lines-fill" /><span>About</span></b-nav-item
         >
         <b-nav-item href="#skills"
         ><b-icon icon="tools" /><span>Skills</span></b-nav-item
         >
         <b-nav-item href="#resume"
-          ><b-icon icon="calendar-range" /><span>Resume</span></b-nav-item
+        ><b-icon icon="calendar-range" /><span>Resume</span></b-nav-item
         >
         <b-nav-item href="#portfolio"
-          ><b-icon icon="columns-gap" /><span>Portfolio</span></b-nav-item
+        ><b-icon icon="columns-gap" /><span>Portfolio</span></b-nav-item
         >
         <b-nav-item href="#blog"
-          ><b-icon icon="files" /><span>Blog</span></b-nav-item
+        ><b-icon icon="files" /><span>Blog</span></b-nav-item
         >
         <b-nav-item href="#contact"
-          ><b-icon icon="envelope" /><span>Contact</span></b-nav-item
+        ><b-icon icon="envelope" /><span>Contact</span></b-nav-item
+        >
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto gnb">
+        <b-nav-item
+          href="https://github.com/Kunie-dev/Kunie-dev.github.io"
+          target="_blank"
+          ><b-icon icon="github" /><span>Github</span></b-nav-item
         >
       </b-navbar-nav>
     </b-navbar>
