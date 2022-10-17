5418d2559342647181a0dbfe11df5cadd515485f
Kunie <kunie.dev@gmail.com>
2022-10-17 21:39:14 +0900

    experience template

--- pages/index.vue
+++ pages/index.vue
@@ -225,6 +225,14 @@
     </MainSection>
     <MainSection id="experience">
       <template #header-text>Experience</template>
+      <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+        <div class="flex-grow-1">
+          <h3 class="mb-0">Senior Web Developer</h3>
+          <div class="subheading mb-3">Intelitec Solutions</div>
+          <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
+        </div>
+        <div class="flex-shrink-0"><span class="text-primary">March 2013 - Present</span></div>
+      </div>
     </MainSection>
     <MainSection id="certification">
       <template #header-text>Certification</template>
